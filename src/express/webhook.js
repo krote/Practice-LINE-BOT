01cb61b73f2995e742a65ce2ea1e75dd253b6d78
krote <krote0924@gmail.com>
2022-09-24 00:33:21 +0900

    add dynamo

--- src/express/webhook.js
+++ src/express/webhook.js
@@ -1,9 +1,11 @@
 // モジュール読み込み
-import line from '@line/bot-sdk';
-import { error, log } from '../log.js';
-import { bot } from '../bot.js';
-import { AppContext } from '../app-context.js';
-import { saveContentFileToDownloadDir } from '../save-file.js';
+import line from "@line/bot-sdk";
+import aws from "aws-sdk";
+import { error, log } from "../log.js";
+import { bot } from "../bot.js";
+import { DynamoDBContext } from "../db.js";
+import { AppContext } from "../app-context.js";
+import { saveContentFileToDownloadDir } from "../save-file.js";
 
 const { CHANNEL_ACCESS_TOKEN } = process.env;
 
@@ -13,6 +15,15 @@ export const webhook = (req, res) => {
 
   // リクエストボディからイベントを取り出し
   const { events } = req.body;
+
+  // DynamoDB DocumentClientのインスタンスを生成
+  const dynamoDocument = new aws.DynamoDB.DocumentClient({
+    endpoint: "http://localhost:8000",
+    region: "ap-northeast-1",
+  });
+
+  // DynamoDB Contextを作成
+  const dynamoDBContext = new DynamoDBContext(dynamoDocument);
 
   // bot-sdkのクライアントを作成
   const lineClient = new line.Client({
@@ -24,15 +35,15 @@ export const webhook = (req, res) => {
 
   // AppContextを作成
   const appContext = new AppContext({
+    dynamoDBContext,
     lineClient,
     contentFileDownloader,
   });
 
   // イベントを処理する関数を呼び出す
-  Promise.all(bot(events, appContext))
-    .catch((err) => {
-      error(`返信処理でエラーが発生しました: ${err}`);
-    });
+  Promise.all(bot(events, appContext)).catch((err) => {
+    error(`返信処理でエラーが発生しました: ${err}`);
+  });
 
-  return res.json('ok');
+  return res.json("ok");
 };
