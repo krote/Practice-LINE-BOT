81f386b1e88f20f5df536e5cea398d6a585aa88f
krote <krote0924@gmail.com>
2022-09-30 23:53:23 +0900

    env setting change

--- src/event/message/news-manage.js
+++ src/event/message/news-manage.js
@@ -3,7 +3,7 @@ import { get } from '../../request.js';
 export const readNews = async () => {
   const newsData = (
     await get(
-      `https://newsapi.org/v2/top-headlines?country=jp&apiKey=${process.env.API_KEY}&pageSize=5`
+      `https://newsapi.org/v2/top-headlines?country=jp&apiKey=${process.env.NEWS_API_KEY}&pageSize=5`
     )
   ).data;
   return newsData;
